2140a1aa65dae236e21576906f4b3e834d027ca8
bgulbis <brian.gulbis@gmail.com>
2015-10-06 14:15:52 -0500

    completed basic shiny app

--- server.R
+++ server.R
@@ -1,11 +1,18 @@
 library(shiny)
 
-model <- lm(mpg ~ hp + wt, data=mtcars)
-change.hp <- summary(model.b)$coefficients[2,1]
-change.wt <- summary(model.b)$coefficients[3,1]
+autoMPG <- function(hp, wt) {
+    model <- lm(mpg ~ hp + wt, data=mtcars)
+    start.mpg <- summary(model)$coefficients[1,1]
+    change.hp <- summary(model)$coefficients[2,1]
+    change.wt <- summary(model)$coefficients[3,1]
+    
+    start.mpg + change.hp * hp + change.wt * wt
+}
 
 shinyServer(
     function(input, output) {
-        
+        output$ohp <- renderPrint({input$hp})
+        output$owt <- renderPrint({input$wt})
+        output$mpg <- renderPrint({autoMPG(input$hp, input$wt)})
     }
 )
--- ui.R
+++ ui.R
@@ -4,10 +4,19 @@ shinyUI(pageWithSidebar(
     headerPanel("Predicting Automobile MPG"),
     sidebarPanel(
         h3("Auto Characteristics"),
-        sliderInput('hp', 'Pick the HP', value=140, min=50, max=340, step=10),
-        sliderInput('wt', 'Pick the Weight (tons)', value=3.5, min=1.5, max=5.5, step=0.5)
+        sliderInput('hp', 'Pick the vehicle Horsepower', value=140, min=50, max=340, step=10),
+        sliderInput('wt', 'Pick the vehicle Weight (in tons)', value=3.5, min=1.5, max=5.5, step=0.5)
     ),
     mainPanel(
-        p("Output goes here")
+        p("Using the mtcars data set, this Shiny app will predict a vehicle's MPG 
+          rating based on the horsepower and weight values. Use the sliders to 
+          adjust the horsepower and weight values and see the effects on the MPG."),
+        h3("You selected:"),
+        h4("Horsepower: "),
+        verbatimTextOutput("ohp"),
+        h4("Weight: "),
+        verbatimTextOutput("owt"),
+        h3("The predicted MPG for this automobile is:"),
+        verbatimTextOutput("mpg")
     )
 ))
