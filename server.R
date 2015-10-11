719aa1cacf8d159a2a74f1b1308a7d56e4edbe46
bgulbis <brian.gulbis@gmail.com>
2015-10-11 13:34:56 -0500

    updated model

--- server.R
+++ server.R
@@ -1,18 +1,29 @@
 library(shiny)
 
-autoMPG <- function(hp, wt) {
-    model <- lm(mpg ~ hp + wt, data=mtcars)
+autoMPG <- function(hp, wt, cyl) {
+    model <- lm(mpg ~ hp + wt + factor(cyl), data=mtcars)
     start.mpg <- summary(model)$coefficients[1,1]
     change.hp <- summary(model)$coefficients[2,1]
     change.wt <- summary(model)$coefficients[3,1]
+    change.cyl6 <- summary(model)$coefficients[4,1]
+    change.cyl8 <- summary(model)$coefficients[5,1]
     
-    start.mpg + change.hp * hp + change.wt * wt
+    change.cyl <- 0
+    
+    if(cyl == 6) {
+        change.cyl <- change.cyl6
+    } else if(cyl == 8) {
+        change.cyl <- change.cyl6 + change.cyl8
+    }
+    
+    start.mpg + change.hp * hp + change.wt * wt + change.cyl
 }
 
 shinyServer(
     function(input, output) {
         output$ohp <- renderText({paste("Horsepower: ", input$hp)})
         output$owt <- renderText({paste("Weight: ", input$wt, " tons")})
-        output$mpg <- renderText({paste(round(autoMPG(input$hp, input$wt), 3), " miles per gallon")})
+        output$ocyl <- renderText({paste("Number of Cylinders: ", input$cyl)})
+        output$mpg <- renderText({paste(round(autoMPG(input$hp, input$wt, input$cyl), 3), " miles per gallon")})
     }
 )
--- ui.R
+++ ui.R
@@ -1,22 +1,50 @@
 library(shiny)
 
 shinyUI(fluidPage(
-    titlePanel("Predicting Automobile MPG"),
+    titlePanel("Predicting Automobile Fuel Efficiency"),
     sidebarLayout(
         sidebarPanel(
             h3("Auto Characteristics"),
-            helpText("Use the sliders to adjust the horsepower and weight values and see the effects on the MPG."),
+            helpText("Use the sliders to adjust the horsepower and weight values, 
+                     or the radio button to select the number of cylinders."),
             sliderInput("hp", "Horsepower", value=200, min=60, max=340, step=20),
-            sliderInput("wt", "Weight (in tons)", value=3.5, min=1.5, max=5.5, step=0.5)
+            sliderInput("wt", "Weight (in tons)", value=3.5, min=1.5, max=5.5, step=0.5),
+            radioButtons("cyl", label = p("Number of Cylinders"),
+                         choices = list("4 Cylinders" = 4, "6 Cylinders" = 6,
+                                        "8 Cylinders" = 8),selected = 4)
         ),
         mainPanel(
-            p("Using the mtcars data set, this Shiny app will predict a vehicle's MPG 
-              rating based on the horsepower and weight values."),
-            h3("Your vehicle settings"),
-            textOutput("ohp"),
-            textOutput("owt"),
-            h3("Predicted fuel efficiency"),
-            textOutput("mpg")
+            tabsetPanel(
+                tabPanel("Output",
+                         h3("Your vehicle settings"),
+                         textOutput("ohp"),
+                         textOutput("owt"),
+                         textOutput("ocyl"),
+                         h3("Predicted fuel efficiency"),
+                         textOutput("mpg")
+                ),
+                tabPanel("Help", 
+                         h4("Purpose of this app"),
+                         p("Recent events have revealed that some automobile manufacturers 
+                           have created certain modifications to their vehicles which 
+                           resulted in falsely-high fuel efficiency ratings during 
+                           government testing. As a results, consumers may be left 
+                           wondering how accurate the information provided by the 
+                           manufacturers really is. This app uses a prediction model 
+                           based upon the mtcars data set to estimate a vehicle's fuel 
+                           efficiency (measured in miles per gallon, or MPG), using 
+                           only a vehicle's gross horsepower, weight, and the number of cylinders."),
+                         h4("How to use this app"),
+                         p("To estimate a vehicle's fuel efficiency, input the automobile's 
+                           horsepower and weight (in tons) using the sliders in the 
+                           Auto Characteristics panel. The number of cylinders can be 
+                           adjusted using the radio buttons. The estimated fuel efficiency 
+                           (in MPG) will be calculated and displayed under the Output tab. 
+                           As you adjust the input values, the estimated fuel efficiency 
+                           will automatically update based upon the new values.")
+                    
+                )
             )
+        )
     )
 ))
